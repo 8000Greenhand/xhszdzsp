3eb7c2d87728ea5c900158a70edb151953855215
时光 <55068633+8000Greenhand@users.noreply.github.com>
2026-06-03 18:48:03 +0800

    feat: register vertical and landscape compositions

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,15 +1,26 @@
 import {Composition} from 'remotion';
 import {AiLearningVideo, videoConfig} from './Video';
+import {LandscapeTutorialVideo, landscapeVideoConfig} from './LandscapeVideo';
 
 export const Root = () => {
   return (
-    <Composition
-      id="AiLearningVideo"
-      component={AiLearningVideo}
-      durationInFrames={videoConfig.durationInFrames}
-      fps={videoConfig.fps}
-      width={videoConfig.width}
-      height={videoConfig.height}
-    />
+    <>
+      <Composition
+        id="VerticalShortVideo"
+        component={AiLearningVideo}
+        durationInFrames={videoConfig.durationInFrames}
+        fps={videoConfig.fps}
+        width={videoConfig.width}
+        height={videoConfig.height}
+      />
+      <Composition
+        id="LandscapeTutorialVideo"
+        component={LandscapeTutorialVideo}
+        durationInFrames={landscapeVideoConfig.durationInFrames}
+        fps={landscapeVideoConfig.fps}
+        width={landscapeVideoConfig.width}
+        height={landscapeVideoConfig.height}
+      />
+    </>
   );
 };
